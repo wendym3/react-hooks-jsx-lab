9be1d451bbdf6b17468298ae6ce2ee0435bc0d54
wendymasese <wendy.masese@student.moringaschool.com>
2024-05-20 16:45:54 +0300

    complete lab

--- src/components/About.js
+++ src/components/About.js
@@ -2,7 +2,12 @@ import React from "react";
 import { image } from "../data/data";
 
 function About() {
-  return <div>About</div>;
+  return  <div id="about">
+     <h2>About Me</h2>
+     <p> I am passionate about web development and enjoy creating interactive and dynamic web applications.</p>
+     <img src={image} alt="I made this" />     
+
+  </div>;
 }
 
 export default About;
